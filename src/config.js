68e8b3297097e0f820c380a453b47fec5fcfbf12
Derenash <eyer.derenash@gmail.com>
2024-08-05 21:49:10 -0300

    changed url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  API_URL: 'http://18.222.164.135:3000',
-  SOCKET_URL: 'http://18.222.164.135:3000',
+  API_URL: 'https://www.derenash.com',
+  SOCKET_URL: 'https://www.derenash.com',
 };
 
 export default config;
